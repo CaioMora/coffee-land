786fa8399095d7c7055467b7c69947062591d8e0
Caio Mora <mora@carnaubatecnologia.io>
2025-10-22 21:29:23 -0300

    (feat) detail-item finish and start order component

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [MatIconModule, MatDividerModule],
+  imports: [MatIconModule, MatDividerModule, MatTabsModule],
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss'
 })
 export class OrderComponent {
-
+  selectedIndex = 0;
 }
